16e37498dc62361b43adc797f001ad2cb831730e
Jose Ledo <a21josevl@iessanclemente.net>
2026-03-29 20:31:40 +0200

    almacenamiento de archivos

--- 12_storage_example/app/Http/Controllers/InfoController.php
+++ 12_storage_example/app/Http/Controllers/InfoController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\InfoRequest;
 use App\Models\Info;
 
@@ -28,6 +29,10 @@ class InfoController extends Controller
         //mover al storage, no a public
         //si el subdirectorio images no existe, se crea automáticamente
         $request->file->storeAs('images', $filename, 'public');
+
+        // otra forma de guardar un archivo en el disco local o s3 o ftp o sftp
+        // Storage::disk('local')->put();
+
         //almacenar en la BD
         $info = new Info;
         $info->name = $request->name;
@@ -37,6 +42,14 @@ class InfoController extends Controller
         //y construir la ruta a posteriori
         $info->file_uri = $filename;
         $info->save();
+
+         // asi tambien podemos hacer para que puedan descargar un archivo
+        // return Storage::download('descarga.jpg', $info->file_url);
+
+        //obtener URL de acceso al archivo:
+        Storage::url($info->file_uri);
+         //url temporal
+         Storage::temporaryUrl('my_image.jpg', now()->addMinutes(10));
 
         return redirect()->route('index');
     }
